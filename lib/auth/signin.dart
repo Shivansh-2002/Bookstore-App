f48f769526b3724540b7c5957a86f5d79d4fb322
Shivansh <shivanshj2002@gmail.com>
2023-08-28 13:24:03 +0530

    making the sign up page responsive

--- lib/auth/signin.dart
+++ lib/auth/signin.dart
@@ -54,74 +54,78 @@ class _SignInScreenState extends State<SignInScreen> {
                   const SizedBox(height: 20,), // SizedBox
 
                   reuseableTextField("Enter Password", Icons.lock_outline, true, _passwordTextController),
-                Container(
-                  width: MediaQuery.of (context).size.width,
-                  height: MediaQuery.of (context).size.height*0.18,
-                  margin: const EdgeInsets. fromLTRB(0, 10, 0, 20),
-                  decoration: BoxDecoration (borderRadius: BorderRadius.circular (90)),
-                  child: Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: ()async{
+                SingleChildScrollView(
+                  child: Container(
+                    width: MediaQuery.of (context).size.width,
+                    height: MediaQuery.of (context).size.height*0.18,
+                    margin: const EdgeInsets. fromLTRB(0, 10, 0, 20),
+                    decoration: BoxDecoration (borderRadius: BorderRadius.circular (90)),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          ElevatedButton(
+                            onPressed: ()async{
 
-                          print("I am here");
-                          FirebaseAuth _auth = FirebaseAuth.instance;
-                          String email = _emailTextController.text;
-                          String password = _passwordTextController.text;
-                          print(email+" "+password);
+                              print("I am here");
+                              FirebaseAuth _auth = FirebaseAuth.instance;
+                              String email = _emailTextController.text;
+                              String password = _passwordTextController.text;
+                              print(email+" "+password);
 
-                          try {
-                            UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-                              email: email,
-                              password: password,
-                            );
-                            print("User registered: ${userCredential.user?.uid}");
-                            // ignore: use_build_context_synchronously
-                            Navigator.push(context,
-                                MaterialPageRoute(builder: (context) => BookstoreHomePage()));
-                          } catch (e) {
-                            print("Error registering user: $e");
-                            setState(() {
-                              showError = true;
-                            });
-                            print(showError);
-                          }
-                        },
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.resolveWith((states) {
-                              if(states.contains (MaterialState.pressed)) {
-                                return Colors.black26;
+                              try {
+                                UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+                                  email: email,
+                                  password: password,
+                                );
+                                print("User registered: ${userCredential.user?.uid}");
+                                // ignore: use_build_context_synchronously
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => BookstoreHomePage()));
+                              } catch (e) {
+                                print("Error registering user: $e");
+                                setState(() {
+                                  showError = true;
+                                });
+                                print(showError);
                               }
-                              return Colors.white;
-                            }),
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder (borderRadius: BorderRadius.circular (30))
-                            )
-                        ),
-                        child: Text('LOG IN' ,
-                          style: const TextStyle(
-                              color: Colors.black87,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16
+                            },
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.resolveWith((states) {
+                                  if(states.contains (MaterialState.pressed)) {
+                                    return Colors.black26;
+                                  }
+                                  return Colors.white;
+                                }),
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder (borderRadius: BorderRadius.circular (30))
+                                )
+                            ),
+                            child: Text('LOG IN' ,
+                              style: const TextStyle(
+                                  color: Colors.black87,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0,3, 0,4),
-                        child: Visibility(
-                          visible: showError, // Set this to the value that controls visibility.
-                          child: Text(
-                            "Invalid email or password.",
-                            style: TextStyle(fontSize: 18,color: Colors.red[900]),
-                            textAlign: TextAlign.center,
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0,3, 0,2),
+                            child: Visibility(
+                              visible: showError, // Set this to the value that controls visibility.
+                              child: Text(
+                                "Invalid email or password.",
+                                style: TextStyle(fontSize: 18,color: Colors.red[900]),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
                           ),
-                        ),
+                          signUpOption(context, true),
+                        ],
                       ),
-                      signUpOption(context, true),
-                    ],
+                    ),
+
+
                   ),
-
-
                 ),
                 ],
               ),
